3525446643960eed24a4c0d4a3005ba0c92c2975
Rachina66 <Tiffinymoore66@gmail.com>
2026-01-11 13:06:04 +0545

    Initial commit for PCOS frontend

---
+++ lib/models/auth/user_model.dart
@@ -0,0 +1,45 @@
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String role;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.role,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+      role: json['role'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'role': role,
+    };
+  }
+
+  User copyWith({
+    String? id,
+    String? name,
+    String? email,
+    String? role,
+  }) {
+    return User(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      role: role ?? this.role,
+    );
+  }
+}
